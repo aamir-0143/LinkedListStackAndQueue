d04f38e36d0ab58005b3de7f440487210a64c4ae
Md aamir reza <aamirreza156@gamil.com>
2021-12-13 18:23:58 +0530

    [Aamir14] Add : Added LinkedListToCreateStack

---
+++ LinkedListStackAndQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LinkedListStackAndQueue
+{ 
+
+
+    class Program
+    {
+        static void Main(string[] agrs) 
+        {
+            Console.WriteLine("Hello World !");
+
+            LinkedListStack LinkedListStack = new LinkedListStack(); 
+            LinkedListStack.Push(70);
+            LinkedListStack.Push(30);
+            LinkedListStack.Push(56);
+            LinkedListStack.Display();
+        }
+    
+    }
+}
