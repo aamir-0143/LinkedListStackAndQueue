602da401e96df04410bb4aba12ae151c4367237b
Md aamir reza <aamirreza156@gamil.com>
2021-12-13 20:31:11 +0530

    [Aamir14] Add : Added LinkedListPeekPopEmpty

--- LinkedListStackAndQueue/Program.cs
+++ LinkedListStackAndQueue/Program.cs
@@ -9,7 +9,6 @@ namespace LinkedListStackAndQueue
         static void Main(string[] agrs) 
         {
             Console.WriteLine("Hello World !");
-
             LinkedListStack linkedListStack = new LinkedListStack();
             linkedListStack.Push(70);
             linkedListStack.Push(30);
